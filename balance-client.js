5e17c6056dd77d083b8e72641527e2ede05bc74a
cjihrig <cjihrig@gmail.com>
2015-12-03 16:48:18 -0500

    code cleanup. coverage to 99%

--- balance-client.js
+++ balance-client.js
@@ -6,18 +6,15 @@
 'use strict'
 
 var _ = require('lodash')
-var Jsonic = require('jsonic')
 
 
 module.exports = function (options) {
   var seneca = this
-
-  // merge default options with any provided by the caller
-  options = seneca.util.deepextend({
-  }, options)
-
-
   var target_map = {}
+  var tu = seneca.export('transport/utils')
+
+  // Merge default options with any provided by the caller
+  options = seneca.util.deepextend({}, options)
 
   seneca.options({
     transport: {
@@ -29,79 +26,68 @@ module.exports = function (options) {
     }
   })
 
-  function add_target( pat, action ) {
-    var patkey = make_patkey( pat )
-    var targetdesc = target_map[patkey]
-
-    targetdesc = targetdesc || { index: 0, targets: [] }
-    target_map[patkey] = targetdesc
-
-    targetdesc.targets.push( { action: action, id: action.id } )
-
-    //console.log( 'add_target', pat, patkey, action.id, targetdesc )
-  }
-
-
-  function remove_target( pat, action_id ) {
-    var patkey = make_patkey( pat )
-    var targetdesc = target_map[patkey]
-
-    targetdesc = targetdesc || { index: 0, targets: [] }
-    target_map[patkey] = targetdesc
-
-    for( var i = 0; i < targetdesc.targets.length; i++ ) {
-      if( action_id === targetdesc.targets[i].id ) {
-        break
-      }
-    }
-
-    if( i < targetdesc.targets.length ) {
-      targetdesc.targets.splice(i,1)
-    }
-
-    //console.log( 'remove_target', pat, patkey, action_id, targetdesc )
-  }
-
-
-  function make_patkey ( pat ) {
-
-    // TOOD: sort!
-    if( _.isString( pat ) ) {
-      return pat
-    }
-
-    var keys = _.keys(seneca.util.clean(pat)).sort()
-    var cleanpat = {}
-    _.each( keys, function (k) {
-      cleanpat[k] = pat[k]
-    })
-
-    //var patkey = Jsonic.stringify( cleanpat )
-    var patkey = seneca.util.pattern( cleanpat )
-    return patkey
-  }
-
-
-  var tu = seneca.export('transport/utils')
-
   seneca.add({
     role: 'transport', hook: 'client', type: 'balance'
   }, hook_client)
 
-
   seneca.add({
     role: 'transport', type: 'balance', add: 'client'
   }, add_client)
-
 
   seneca.add({
     role: 'transport', type: 'balance', remove: 'client'
   }, remove_client)
 
 
+  function add_target ( pat, action ) {
+    var patkey = make_patkey( pat )
+    var targetdesc = target_map[patkey]
+
+    targetdesc = targetdesc || { index: 0, targets: [] }
+    target_map[patkey] = targetdesc
+    targetdesc.targets.push( { action: action, id: action.id } )
+  }
+
+
+  function remove_target ( pat, action_id ) {
+    var patkey = make_patkey( pat )
+    var targetdesc = target_map[patkey]
+
+    targetdesc = targetdesc || { index: 0, targets: [] }
+    target_map[patkey] = targetdesc
+
+    for ( var i = 0; i < targetdesc.targets.length; i++ ) {
+      if ( action_id === targetdesc.targets[i].id ) {
+        break
+      }
+    }
+
+    if ( i < targetdesc.targets.length ) {
+      targetdesc.targets.splice(i, 1)
+    }
+  }
+
+
+  function make_patkey ( pat ) {
+    // TODO: sort!
+    if ( _.isString( pat ) ) {
+      return pat
+    }
+
+    var keys = _.keys(seneca.util.clean(pat)).sort()
+    var cleanpat = {}
+
+    _.each( keys, function (k) {
+      cleanpat[k] = pat[k]
+    })
+
+    var patkey = seneca.util.pattern( cleanpat )
+    return patkey
+  }
+
 
   function add_client (msg, done) {
-    if( !msg.config.id ) {
+    if ( !msg.config.id ) {
       msg.config.id = this.util.pattern( msg.config )
     }
 
@@ -111,57 +97,43 @@ module.exports = function (options) {
 
 
   function remove_client (msg, done) {
-    if( !msg.config.id ) {
+    if ( !msg.config.id ) {
       msg.config.id = this.util.pattern( msg.config )
     }
 
     // TODO: what about array of pins?
-    var patkey = make_patkey( msg.config.pin )    
-    remove_target( patkey, msg.config.id )
+    var patkey = make_patkey( msg.config.pin )
 
+    remove_target( patkey, msg.config.id )
     done()
   }
-  
-
 
 
   function hook_client (args, clientdone) {
-    var seneca = this
     var type = args.type
     var client_options = seneca.util.clean(_.extend({}, options[type], args))
-    
-    tu.make_client(make_send, client_options, clientdone)
 
-    var index = -1
+    tu.make_client(make_send, client_options, clientdone)
 
     function make_send (spec, topic, send_done) {
       seneca.log.debug('client', 'send', topic + '_res', client_options, seneca)
 
       send_done(null, function (args, done) {
-        var patkey  = args.meta$.pattern
+        var patkey = args.meta$.pattern
         var targetdesc = target_map[patkey]
- 
-        //console.log( 'send', patkey, targetdesc )
 
-        if( targetdesc ) {
+        if ( targetdesc ) {
           var targets = targetdesc.targets
-          var index   = targetdesc.index
+          var index = targetdesc.index
 
-          if( targetdesc.targets.length <= index ) {
-            index = targetdesc.targets.length - 1
-          }
-          
-          if( targets[index] ) {
+          if ( targets[index] ) {
             targets[index].action.call( this, args, done )
             targetdesc.index = ( index + 1 ) % targets.length
-          }
-          else {
-            done()
+            return
           }
         }
-        else {
-          done()
-        }
+
+        done()
       })
     }
 
@@ -171,4 +143,3 @@ module.exports = function (options) {
     })
   }
 }
-
--- test/balance-client.test.js
+++ test/balance-client.test.js
@@ -13,48 +13,43 @@ var lab = exports.lab = Lab.script()
 var describe = lab.describe
 var it = lab.it
 
+var testopts = {log: 'silent'}
+
 
 describe('#balance-client', function () {
 
   it('happy', function (done) {
-    var s0, s1, c0
-
-    s0 = 
-      Seneca()
+    var s0 =
+      Seneca(testopts)
       .error(done)
       .listen(44440)
-      .add('a:1',function(){this.good({x:0})})
-      .ready( function () {
-      
-        s1 = 
-          Seneca()
+      .add('a:1', function () { this.good({ x: 0 }) })
+      .ready(function () {
+        var s1 =
+          Seneca(testopts)
           .error(done)
           .listen(44441)
-          .add('a:1',function(){this.good({x:1})})
-          .ready( function () {
-      
-            c0 = 
-              Seneca()
+          .add('a:1', function () { this.good({ x: 1 }) })
+          .ready(function () {
+            var c0 =
+              Seneca(testopts)
               .error(done)
               .use('..')
-              .client( {type:'balance', pin:'a:1'} )
-              .client( {port:44440, pin:'a:1'} )
-              .client( {port:44441, pin:'a:1'} )
-              .act('a:1',function(e,o){
-                //console.log(0,e,o)
-                Assert.equal(0,o.x)
+              .client({ type: 'balance', pin: 'a:1' })
+              .client({ port: 44440, pin: 'a:1' })
+              .client({ port: 44441, pin: 'a:1' })
+              .act('a:1', function (e, o) {
+                Assert.equal(0, o.x)
 
-                c0.act('a:1',function(e,o){
-                  //console.log(0,e,o)
-                  Assert.equal(1,o.x)
+                c0.act('a:1', function (e, o) {
+                  Assert.equal(1, o.x)
 
-                  c0.act('a:1',function(e,o){
-                    //console.log(0,e,o)
-                    Assert.equal(0,o.x)
+                  c0.act('a:1', function (e, o) {
+                    Assert.equal(0, o.x)
 
-                    s0.close( function () {
-                      s1.close( function () {
-                        c0.close( function () {
+                    s0.close(function () {
+                      s1.close(function () {
+                        c0.close(function () {
                           done()
                         })
                       })
@@ -68,59 +63,57 @@ describe('#balance-client', function () {
 
 
   it('add-remove', function (done) {
-    var s0, s1, c0
-
-    s0 = 
-      Seneca()
+    var s0 =
+      Seneca(testopts)
       .error(done)
       .listen(44440)
-      .add('a:1',function(){this.good({x:0})})
-      .ready( function () {
-      
-        s1 = 
-          Seneca()
+      .add('a:1', function () { this.good({ x: 0 }) })
+      .ready(function () {
+        var s1 =
+          Seneca(testopts)
           .error(done)
           .listen(44441)
-          .add('a:1',function(){this.good({x:1})})
-          .ready( function () {
-      
-            c0 = 
-              Seneca()
+          .add('a:1', function () { this.good({ x: 1 }) })
+          .ready(function () {
+            var c0 =
+              Seneca(testopts)
               .error(done)
               .use('..')
-              .client( {type:'balance', pin:'a:1'} )
-              .act( 
-                'role:transport,type:balance,add:client', 
-                {config:{port:44440, pin:'a:1'}}, 
+              .client({ type: 'balance', pin: 'a:1' })
+              .act(
+                'role:transport,type:balance,add:client',
+                { config: { port: 44440, pin: 'a:1' } },
                 function () {
+                  c0.act('a:1', function (e, o) {
+                    Assert.equal(0, o.x)
 
-                  c0.act('a:1',function(e,o){
-                    //console.log(0,e,o)
-                    Assert.equal(0,o.x)
-
-                    c0.act( 
-                      'role:transport,type:balance,add:client', 
-                      {config:{port:44441, pin:'a:1'}}, 
+                    c0.act(
+                      'role:transport,type:balance,add:client',
+                      { config: { port: 44441, pin: 'a:1' } },
                       function () {
+                        c0.act('a:1', function (e, o) {
+                          Assert.equal(0, o.x)
 
-                        c0.act('a:1',function(e,o){
-                          Assert.equal(0,o.x)
+                          c0.act('a:1', function (e, o) {
+                            Assert.equal(1, o.x)
 
-                          c0.act('a:1',function(e,o){
-                            Assert.equal(1,o.x)
-
-                            c0.act( 
-                              'role:transport,type:balance,remove:client', 
-                              {config:{port:44440, pin:'a:1'}}, 
+                            c0.act(
+                              'role:transport,type:balance,remove:client',
+                              { config: { port: 44441, pin: 'a:1' } },
                               function () {
-                                
-                                c0.act('a:1',function(e,o){
-                                  Assert.equal(1,o.x)
+                                c0.act('a:1', function (e, o) {
+                                  Assert.equal(0, o.x)
 
-                                  c0.act('a:1',function(e,o){
-                                    Assert.equal(1,o.x)
+                                  c0.act('a:1', function (e, o) {
+                                    Assert.equal(0, o.x)
 
-                                    done()
+                                    s0.close(function () {
+                                      s1.close(function () {
+                                        c0.close(function () {
+                                          done()
+                                        })
+                                      })
+                                    })
                                   })
                                 })
                               })
@@ -130,6 +123,150 @@ describe('#balance-client', function () {
                   })
                 })
           })
+      })
+  })
+
+
+  it('doesn\'t remove when no match is found', function (done) {
+    var s0 =
+      Seneca(testopts)
+      .error(done)
+      .listen(44440)
+      .add('a:1', function () { this.good({ x: 0 }) })
+      .ready(function () {
+        var s1 =
+          Seneca(testopts)
+          .error(done)
+          .listen(44441)
+          .add('a:1', function () { this.good({ x: 1 }) })
+          .ready(function () {
+            var c0 =
+              Seneca(testopts)
+              .error(done)
+              .use('..')
+              .client({ type: 'balance', pin: 'a:1' })
+              .act(
+                'role:transport,type:balance,add:client',
+                { config: { port: 44440, pin: 'a:1' } },
+                function () {
+                  c0.act('a:1', function (e, o) {
+                    Assert.equal(0, o.x)
+
+                    c0.act(
+                      'role:transport,type:balance,add:client',
+                      { config: { port: 44441, pin: 'a:1' } },
+                      function () {
+                        c0.act('a:1', function (e, o) {
+                          Assert.equal(0, o.x)
+
+                          c0.act('a:1', function (e, o) {
+                            Assert.equal(1, o.x)
+
+                            c0.act(
+                              'role:transport,type:balance,remove:client',
+                              { config: { port: 44440, pin: 'a:5' } },
+                              function () {
+                                c0.act('a:1', function (e, o) {
+                                  Assert.equal(0, o.x)
+
+                                  c0.act('a:1', function (e, o) {
+                                    Assert.equal(1, o.x)
+
+                                    s0.close(function () {
+                                      s1.close(function () {
+                                        c0.close(function () {
+                                          done()
+                                        })
+                                      })
+                                    })
+                                  })
+                                })
+                              })
+                          })
+                        })
+                      })
+                  })
+                })
+          })
+      })
+  })
+
+
+  it('uses a custom id when adding and removing clients', function (done) {
+    var s0 =
+      Seneca(testopts)
+      .error(done)
+      .listen(44440)
+      .add('a:1', function () { this.good({ x: 0 }) })
+      .ready(function () {
+        var s1 =
+          Seneca(testopts)
+          .error(done)
+          .listen(44441)
+          .add('a:1', function () { this.good({ x: 1 }) })
+          .ready(function () {
+            var c0 =
+              Seneca(testopts)
+              .error(done)
+              .use('..')
+              .client({ type: 'balance', pin: 'a:1' })
+              .act(
+                'role:transport,type:balance,add:client',
+                { config: { port: 44440, pin: 'a:1', id: 'foo' } },
+                function () {
+                  c0.act('a:1', function (e, o) {
+                    Assert.equal(0, o.x)
+
+                    c0.act(
+                      'role:transport,type:balance,add:client',
+                      { config: { port: 44441, pin: 'a:1', id: 'bar' } },
+                      function () {
+                        c0.act('a:1', function (e, o) {
+                          Assert.equal(0, o.x)
+
+                          c0.act('a:1', function (e, o) {
+                            Assert.equal(1, o.x)
+
+                            c0.act(
+                              'role:transport,type:balance,remove:client',
+                              { config: { id: 'bar', pin: 'a:1' } },
+                              function () {
+                                c0.act('a:1', function (e, o) {
+                                  Assert.equal(0, o.x)
+
+                                  c0.act('a:1', function (e, o) {
+                                    Assert.equal(0, o.x)
+
+                                    s0.close(function () {
+                                      s1.close(function () {
+                                        c0.close(function () {
+                                          done()
+                                        })
+                                      })
+                                    })
+                                  })
+                                })
+                              })
+                          })
+                        })
+                      })
+                  })
+                })
+          })
+      })
+  })
+
+
+  it('processes actions even when no upstreams are available', function (done) {
+    var c0 =
+      Seneca(testopts)
+      .error(done)
+      .use('..')
+      .client({ type: 'balance', pin: 'a:1' })
+      .act('a:1', function (e, o) {
+        Assert.strictEqual(e, null)
+        Assert.strictEqual(o, undefined)
+        c0.close(done)
       })
   })
 })
